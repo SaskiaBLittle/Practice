9fd2aa1a7d58508f78d010a1f007a49f5ee2a3a1
Saskia Brienesse-Little (1000125002) <BRIESG1@student.op.ac.nz>
2024-09-10 12:59:51 +1200

    Put text in correct place

--- Stuff/JSPokemon.js
+++ Stuff/JSPokemon.js
@@ -14,6 +14,7 @@ function loopPokes(pokes) {
     for (pokemon of pokes.results) {
         const poke = document.createElement("div");
 
+        //displays name and when clicked will display link/call lin in the inspect console
         poke.innerHTML = `Name: ${pokemon.name}`;
         poke.url = pokemon.url;
         document.querySelector("body").appendChild(poke);
@@ -24,7 +25,28 @@ function loopPokes(pokes) {
 async function getMyPokemon(e) {
     const res = await fetch(e.target.url);
     const pokeData = await res.json();
-    console.log(pokeData.name);
+    console.log(pokeData)
+
+    onlyOne(pokeData);
+}
+
+function onlyOne(f) {
+    
+
+    const one = document.createElement("div");
+    const pokeImg = document.createElement("img");
+
+    pokeImg.src = f.sprites.front_default;
+    for (stats of f.stats) {
+        const stat = document.createElement("h3");
+
+        stat.innerHTML = `${stats.stat.name}: ${stats.base_stat}`;
+
+        document.querySelector("body").appendChild(stat);
+    }
+
+    one.appendChild(pokeImg);
+    document.querySelector("body").appendChild(one);
 }
 
 getPokes();
